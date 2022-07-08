acb65c4e051987a0106450c416e0ee4a151c3d29
Bryan Medina <bryan.medina.per@gmail.com>
2022-07-07 23:17:34 -0500

    Add logic to use the calculator with a GUI

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-const add = (a, b) => a + b;
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector(".screen>p");
+
+let displayVal = '';
+let previousVal = '0';
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e)=>{
+    if(e.target.className === 'num'){
+      displayVal += e.target.id;
+      display.textContent = displayVal;
+    }
+    else if(e.target.className === 'operator'){
+      displayVal =  `${add(previousVal, displayVal)}`;
+      display.textContent = displayVal;
+      previousVal = displayVal;
+      displayVal = '';
+    }
+    else if(e.target.id === 'equal'){
+      displayVal = `${add(previousVal, displayVal)}`;
+      display.textContent = displayVal;
+      previousVal = displayVal;
+      displayVal = '';
+    }
+  });
+});
+
+
+const add = (a, b) => parseInt(a) + parseInt(b);
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => b !== 0 ? a / b : 'Error';
